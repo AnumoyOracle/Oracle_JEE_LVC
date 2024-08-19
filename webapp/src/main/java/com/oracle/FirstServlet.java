b44c6ff1868183a4ea07eb0996c7c793e16840ea
anumnand <anumnand@localhost>
2024-08-19 15:00:15 +0530

    Initial Commit for this servlet program.

---
+++ webapp/src/main/java/com/oracle/FirstServlet.java
@@ -0,0 +1,22 @@
+package com.oracle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h1>First Servlet</h1>");
+		out.println("<a href='SecondServlet'>Second Servlet</a>");
+		out.close();
+	}
+   
+}
---
+++ webapp/src/main/java/com/oracle/SecondServlet.java
@@ -0,0 +1,21 @@
+package com.oracle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SecondServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h1>Second Servlet</h1>");
+		out.close();
+	}
+
+}
