321ac918a30e571a6ab9b1bf210118c5b206270c
anumnand <anumnand@localhost>
2024-08-23 20:02:00 +0530

    Rest API + MySQL Database

---
+++ restapidemo/src/main/java/com/oracle/restapidemo/RestapidemoApplication.java
@@ -0,0 +1,14 @@
+package com.oracle.restapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapidemoApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
