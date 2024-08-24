c03d0ec0ee50b5e5bd3837d5a7cd8256e840110a
anumnand <anumnand@localhost>
2024-08-24 20:06:17 +0530

    Websocket implementation

---
+++ websocket/src/main/java/com/oracle/websocket/Greeting.java
@@ -0,0 +1,20 @@
+package com.oracle.websocket;
+
+public class Greeting {
+
+	private String message;
+
+	public Greeting() {
+
+	}
+
+	public Greeting(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
---
+++ websocket/src/main/java/com/oracle/websocket/HelloMessage.java
@@ -0,0 +1,20 @@
+package com.oracle.websocket;
+
+public class HelloMessage {
+
+	private String name;
+
+	public HelloMessage() {
+
+	}
+
+	public HelloMessage(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
